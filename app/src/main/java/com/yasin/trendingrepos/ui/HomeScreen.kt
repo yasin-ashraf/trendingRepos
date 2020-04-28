e00f326402fcafc898b9c45a4b024847ab8f2d7c
Yasin Ashraf <yasin.asharf@gmail.com>
2020-04-28 19:54:54 +0530

    set theme

---
+++ app/src/main/java/com/yasin/trendingrepos/ui/HomeScreen.kt
@@ -0,0 +1,21 @@
+package com.yasin.trendingrepos.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Yasin on 28/4/20.
+ */
+class HomeScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
